fc9e75b89e9dc0680d3c47e02bd775f76a24a5cc
sobujcoder <87200928+sobujcoder@users.noreply.github.com>
2024-04-16 02:19:16 +0600

    Add files via upload

--- BRTAController.java
+++ BRTAController.java
@@ -3,12 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,20 +25,6 @@ public class BRTAController implements Initializable {
 
     @FXML
     private Label BRTAlabel;
-    @FXML
-    private Button passengerinfobtn;
-    @FXML
-    private Button publictransitbtn;
-    @FXML
-    private Button speedbtn;
-    @FXML
-    private Button safetyandsecuritybtn;
-    @FXML
-    private Button Emergencypolicebtn;
-    @FXML
-    private Button publictranssitbtn;
-    @FXML
-    private Button logoutbtn;
 
     /**
      * Initializes the controller class.
@@ -41,5 +33,101 @@ public class BRTAController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void passengerinfobtnmouseonclick(ActionEvent event) throws IOException {
+     
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("passengerinformation.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+       }
+    
+
+    @FXML
+    private void publictransitmouseonclick(ActionEvent event) throws IOException {
+        
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Public transit routes.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
+
+    @FXML
+    private void speedbtnmouseonclick(ActionEvent event) throws IOException {
+       
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Speed monitoring.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
+
+    @FXML
+    private void safetyandsecuritymoouseonclick(ActionEvent event) throws IOException {
+        
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("safety and security on public transit.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
+
+    @FXML
+    private void Emergencypolicebtnmouseonclick(ActionEvent event) throws IOException {
+      
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Emergency traffic police support.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
+
+    @FXML
+    private void publictransitroutesmouseonclick(ActionEvent event) throws IOException {
+      
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("Public transit routes.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
+
+    @FXML
+    private void logoutmouseonclick(ActionEvent event) throws IOException {
+     
+        
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("logout.fxml"));
+    Parent dmOrdersceneParent = loader.load();
+    Scene dmOrderscene = new Scene(dmOrdersceneParent);
+    Stage dmOrderstage = (Stage)((Node)event.getSource()).getScene().getWindow(); 
+    dmOrderstage.setScene(dmOrderscene);
+    dmOrderstage.show();
+        
+    }
+    
     
 }
